da32100126b893bd14b907b29d40aff30a67217b
chenbofan <chenbofan@changingedu.com>
2021-05-27 00:47:14 +0800

    init

--- broker/src/main/java/com/cbf/process/ThreeStageProcessor.java
+++ broker/src/main/java/com/cbf/process/ThreeStageProcessor.java
@@ -4,6 +4,10 @@ import com.cbf.intermediateProcess.IntermediateProcess;
 import com.cbf.read.BaseReader;
 import com.cbf.sink.Sinker;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author Sky
  * @version 1.0
@@ -19,11 +23,24 @@ public class ThreeStageProcessor {
         this.reader = reader;
         this. intermediateProcess = intermediateProcess;
     }
+    private final int THREAD_NUMBER = 2;
     //使用一个queue来同步read、intermediaeProcess呢，还是用多个queue呢
     /**
      * 程序主流程
      */
     public void process(){
-
+        try{
+            ExecutorService service = Executors.newFixedThreadPool(THREAD_NUMBER);
+            service.execute(()->read());
+            service.execute(()->setIntermediateProcess());
+            service.execute(()->sink());
+            service.shutdown();
+            service.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+    public void read(){}
+    public void setIntermediateProcess(){}
+    public void sink(){}
 }
